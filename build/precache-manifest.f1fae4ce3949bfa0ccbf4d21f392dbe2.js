9660f85e2b4d8eeee2b88507423bbe17dee4b6e9
40 daimon <tvwmdaimon40@gmail.com>
2019-12-24 15:01:29 -0800

    add test folder

---
+++ build/precache-manifest.f1fae4ce3949bfa0ccbf4d21f392dbe2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ce988613d7eb3c87d2983b09516f19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "981d897c17bf9af93a69",
+    "url": "./static/css/main.03d3db64.chunk.css"
+  },
+  {
+    "revision": "477c8f4d77e9f1598dd1",
+    "url": "./static/js/2.2ea5aa5f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.2ea5aa5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "981d897c17bf9af93a69",
+    "url": "./static/js/main.3e599ea0.chunk.js"
+  },
+  {
+    "revision": "9df3dd1a3de1a2dd8a08",
+    "url": "./static/js/runtime-main.99d08cf3.js"
+  },
+  {
+    "revision": "ccf131681e4d8ad436e7a128b9efc47e",
+    "url": "./static/media/FantuanLogo.ccf13168.png"
+  },
+  {
+    "revision": "550c704eb9d8272460c2f9d91e05e279",
+    "url": "./static/media/chkchshotpot1.550c704e.jpg"
+  },
+  {
+    "revision": "474e46c904322f3a5d451fdcf0212c58",
+    "url": "./static/media/sushi_chef.474e46c9.jpg"
+  },
+  {
+    "revision": "cd68c5575cf51d492437d7e1ab953c69",
+    "url": "./static/media/sushi_promo.cd68c557.mp4"
+  },
+  {
+    "revision": "dfb2fa41177efb966aa863c7795c7de0",
+    "url": "./static/media/sushi_roll.dfb2fa41.jpg"
+  }
+]);
